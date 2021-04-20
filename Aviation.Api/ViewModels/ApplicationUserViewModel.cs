26f7129361646050b778ca281884780bb919165c
thiagowerneck1990@hotmail.com <thiagowerneck1990@hotmail.com>
2021-04-20 16:22:01 -0300

    Enviando userviremodel

---
+++ Aviation.Api/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AviationManagementApi.Api.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; } 
+        public string Telefone { get; set; } 
+        public string Empresa { get; set; }
+        public string EmpresaCnpj { get; set; }
+    }
+}
